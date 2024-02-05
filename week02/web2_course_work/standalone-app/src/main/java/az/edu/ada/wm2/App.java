6ccef90967e8ee12085aa9a97fe12a8cd5ed404e
zumrudisgandar <zzumus@mail.ru>
2024-02-06 01:46:33 +0400

    Maven Standalone and Web Java applications were created. Maven Wrapper installed for both applications.

---
+++ week02/web2_course_work/standalone-app/src/main/java/az/edu/ada/wm2/App.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is my first maven standalone Java Application!" );
+    }
+}
